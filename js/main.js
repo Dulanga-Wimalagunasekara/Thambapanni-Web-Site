79346bfcbd8ad4ca3901de3b9c697da81f9be237
dulangawimalagunasekara <dulanga.wimalagunasekara@arimaclanka.com>
2023-01-07 16:10:41 +0530

    Collapse navbar after clicking

--- js/main.js
+++ js/main.js
@@ -10,21 +10,23 @@
     const carousel = document.getElementById('carousel');
 
     // Dropdown on mouse hover
-    $(document).ready(function () {
-        function toggleNavbarMethod() {
-            if ($(window).width() > 992) {
-                $('.navbar .dropdown').on('mouseover', function () {
-                    $('.dropdown-toggle', this).trigger('click');
-                }).on('mouseout', function () {
-                    $('.dropdown-toggle', this).trigger('click').blur();
-                });
-            } else {
-                $('.navbar .dropdown').off('mouseover').off('mouseout');
-            }
-        }
-        toggleNavbarMethod();
-        $(window).resize(toggleNavbarMethod);
-    });
+    // $(document).ready(function () {
+    //     function toggleNavbarMethod() {
+    //         if ($(window).width() > 992) {
+    //             console.log("Inside IF");
+    //             $('.navbar .dropdown').on('mouseover', function () {
+    //                 $('.dropdown-toggle', this).trigger('click');
+    //             }).on('mouseout', function () {
+    //                 $('.dropdown-toggle', this).trigger('click').blur();
+    //             });
+    //         } else {
+    //             console.log("Inside Else");
+    //             $('.navbar .dropdown').off('mouseover').off('mouseout');
+    //         }
+    //     }
+    //     toggleNavbarMethod();
+    //     $(window).resize(toggleNavbarMethod);
+    // });
 
 
     // Date and time picker
